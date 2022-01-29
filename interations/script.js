b41edd415045c426a0b8f374a2e4fe5b4dfb2043
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-01-29 20:12:58 -0300

    first files

---
+++ interations/script.js
@@ -0,0 +1,14 @@
+var classes = ['comidas','frutas','objetos','meios de transportes','legumes','brinquedos','linguagens de programação','planetas','animais','biomas','instrumentos']
+var elements = [
+    {'class':'comidas','element': []},
+    {'class':'frutas','element': []},
+    {'class':'objetos','element': []},
+    {'class':'meios de transportes','element': []},
+    {'class':'legumes','element': []},
+    {'class':'brinquedos','element': []},
+    {'class':'linguagens de progamação','element': []},
+    {'class':'planetas','element': []},
+    {'class':'animais','element': []},
+    {'class':'biomas','element': []},
+    {'class':'instrumentos','element': []}
+]
