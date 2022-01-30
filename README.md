027b487b2afc2784688837015918626e59f786e5
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-01-30 20:22:05 -0300

    HangMan Game v1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+Hello , everyone .
+
+This is a hangman game made with HTML , CSS and JAVASCRIPT .
+
+How to play :
+
+-You have 7 (seven) lifes to win the game . 
+-For each wrong letter typed you lose a life.
+-If you reach zero lives without finding the word you lose the game.
+-A black div mean a space in the word.
+
+Test my code and tell me what you think about it (gabriel36dantas@gmail.com).
+
+Feel free to modify it or add new features.
+
+Thanks to acess my code and follow-me on github.
--- interations/script.js
+++ interations/script.js
@@ -5,7 +5,7 @@ var elements = [
     {'class':'objetos','element': ['candelabro','fotografia','carregador','banheira','computador','escrivaninha','dinheiro','penteadeire','grampeador','fechadura','ventilador','umidificador'],'color':'#D47C17'},
     {'class':'meios de transportes','element': ['trem-bala','submarino','motocicleta','caminhao','bicicleta','patinete','charrete','veleiro','onibus','transatlantico','navio cargueiro','dirigivel'],'color':'#D6D69D'},
     {'class':'legumes \\ verduras','element': ['cenoura','beterraba','cebola','beringela','Ervilha','Gengibre','inhame','lentilha','mandioquina','pimenta'],'color':'#B0E39A'},
-    {'class':'brinquedos \\ jogos','element': ['boneca','pista de corrida','carro de controle remoto','massa de modelar','pique-esconde','vai e volta','pula-corda','quebra-cabeça','pega-peg','pega varetas','amarelinha'],'color':'#F0E02C'},
+    {'class':'brinquedos \\ jogos','element': ['boneca','pista de corrida','carro de controle remoto','massa de modelar','pique-esconde','vai e volta','pula-corda','quebra-cabeça','pega-pega','pega varetas','amarelinha'],'color':'#F0E02C'},
     {'class':'planetas \\ galaxias','element': ['marte','venus','terra','mercurio','saturno','urano','netuno','andromeda','centauros a','esquimo','nebulosa','via lactea'],'color':'#ED8DA1'},
     {'class':'animais','element': ['bufalo','cachorro','tartaruga marinha','tigre dente de sabres','urso polar','gorila','golfinho','baleia jubarte','crocodilo','camaleão','leão africano','cavalo marinho'],'color':'#D64620'},
     {'class':'biomas','element': ['tundra artica','taiga','floresta tropical','floresta temperada','savana','deserto','campos','chaparral','tundra alpina','selva','caatinga','cerrado'],'color':'#37F069'},
@@ -19,6 +19,7 @@ var word = elements[position].element[Math.round(Math.random()*(elements[positio
 var wordWidth = word.length;
 var lettersBox = [];
 var draws = [];
+var word1 ;
 var maxMistakes = 7;
 var hits;
 var play;
@@ -27,14 +28,26 @@ var right = false;
 var playing = false;
 
 function whatletters(width) {
-    var obj;
     let words = document.querySelectorAll('.word');
     words.forEach(item => {
         item.innerHTML = '';
         item.style.display = 'none';
+        item.style.background = 'rgb(172, 172, 172)';
     });
     for (var i = 1;i<=width;i++){
-        obj = document.querySelector(`#word${i}`).style.display = 'flex';
+        document.querySelector(`#word${i}`).style.display = 'flex';
+    }
+    for(var c = 0;c<word.length;c++){
+        if (word[c] === ' ') {
+            document.querySelector(`#word${c+1}`).style.background = 'black';
+            hits += 1;
+        }
+    }
+    for(var c = 0;c<word.length;c++){
+        if (word[c] === '-') {
+            document.querySelector(`#word${c+1}`).style.innerHTML = '-';
+            hits += 1;
+        }
     }
 };
 
@@ -67,14 +80,14 @@ function playGame() {
                 if (mistakes < 7) {
 
                 }else{
-                    playing = false;document.getElementById('msg').innerHTML = 'Infelizmente você perdeu ):!';
-                    document.getElementById('msg').style.background = 'rgba(255,0,0,0.5)'
+                    playing = false;document.getElementById('msg').innerHTML = 'Infelizmente você perdeu ):! A palavra era : '+word1.toUpperCase()+'.';
+                    document.getElementById('msg').style.background = 'rgba(255,0,0,0.5)';
                 }
             }
             if (hits == wordWidth) {
                 playing = false;
-                document.getElementById('msg').innerHTML = 'Parabéns , você ganhou !';
-                document.getElementById('msg').style.background = 'rgba(0,255,0,0.5)'
+                document.getElementById('msg').innerHTML = 'Parabéns , você ganhou  A palavra era : '+word1.toUpperCase()+'.';
+                document.getElementById('msg').style.background = 'rgba(0,255,0,0.5)';
             }
             if (!playing){
                 document.getElementById('msg').style.display = 'block';
@@ -103,6 +116,7 @@ function startGame() {
     position = Math.round(Math.random()*(elements.length-1));
     usedClass = elements[position].class
     word = elements[position].element[Math.round(Math.random()*(elements[position].element.length-1))];
+    word1 = word;
     wordWidth = word.length;
     whatletters(wordWidth);
     document.getElementById('step').innerHTML = usedClass;
