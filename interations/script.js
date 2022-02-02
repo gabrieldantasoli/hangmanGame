873834859c38d3b91d9b7b9c53abaea5cf18b04e
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-02-02 11:54:06 -0300

    HangMan Game

--- interations/script.js
+++ interations/script.js
@@ -1,4 +1,4 @@
-var classes = ['comidas','frutas','objetos','meios de transportes','legumes','brinquedos','linguagens de programação','planetas','animais','biomas','instrumentos']
+var classes = ['comidas','frutas','objetos','meios de transportes','legumes','brinquedos','linguagens de programação','planetas','animais','biomas','instrumentos'];
 var elements = [
     {'class':'comidas','element': ['Coxinha','lasanha','acaraje','feijoada','panqueca','canjica','churrasco','parmegiana','estrogonofe','hamburger','cachorro quente','macarronada'],'color':'#EB2F22'},
     {'class':'frutas','element': ['jaboticaba','graviola','araticum','alfarroba','bergamota','framboesa','guabiroba','ibacurupari','macauba','pitangatuba','seriguela','tamarindo'],'color':'#27EB24'},
@@ -10,105 +10,66 @@ var elements = [
     {'class':'animais','element': ['bufalo','cachorro','tartaruga marinha','tigre dente de sabres','urso polar','gorila','golfinho','baleia jubarte','crocodilo','camaleao','leao africano','cavalo marinho'],'color':'#D64620'},
     {'class':'biomas','element': ['tundra artica','taiga','floresta tropical','floresta temperada','savana','deserto','campos','chaparral','tundra alpina','selva','caatinga','cerrado'],'color':'#37F069'},
     {'class':'instrumentos','element': ['saxofone','violino','flauta','clarinete','bateria','tambor','clarone','viola','guitarra','contra baixo','berimbal','violoncelo'],'color':'#daa520'}
-]
+];
 
-var allClasses = elements.length-1;
-var position = Math.round(Math.random()*(elements.length-1));
-var usedClass = elements[position].class;
-var word = elements[position].element[Math.round(Math.random()*(elements[position].element.length-1))];
-var wordWidth = word.length;
-var lettersBox = [];
-var draws = [];
-var word1 ;
-var maxMistakes = 7;
-var hits;
-var play;
-var mistakes = 0;
-var right = false;
-var playing = false;
+var allClasses = elements.length - 1;
+var hight = false ;
+var playing = false ;
+var usedLettters = [] ;
+var maxMistakes = 7 ;
+var position ;
+var word ;
+var wordWidth ;
+var hits ;
+var mistakes ;
+var usedLetters ;
+var play ;
+var clas ;
+var wordPosition ;
+var obj ;
 
-function whatletters(width) {
-    let words = document.querySelectorAll('.word');
-    words.forEach(item => {
-        item.innerHTML = '';
-        item.style.display = 'none';
-        item.style.background = 'rgb(172, 172, 172)';
+function wordLetters(word) {
+    obj = document.querySelectorAll('.word') ;
+    obj.forEach(item => {
+        item.style.display = 'none' ;
+        item.style.background = 'rgb(172, 172, 172)' ;
+        item.innerHTML = '' ;
     });
-    for (var i = 1;i<=width;i++){
-        document.querySelector(`#word${i}`).style.display = 'flex';
-    }
+    for (var i = 0 ; i < word.length ; i++ ) {
+        obj[i].style.display = 'flex' ;
+        if (word[i] == ' ') {
+            obj[i].style.background = '#000' ;
+            hits += 1 ;
+        };
+        if (word[i] == '-') {
+            obj[i].innerHTML = '-' ;
+            hits += 1 ;
+        };
+    };
 };
 
 function playGame() {
-    play.focus();
-    if (play.value == '') {
-        window.alert('Digite uma letra !')
-    }else{
-        if (playing) {
-            var obj ;
-            var lettertm ;
-            var letter ;
-            var search ;
-            letter = play.value;
-            play.value = '';
-            right = false;
-            word = word.toLowerCase()
-            search = word.match(letter.toLowerCase());
-            while (search != null) {
-                lettertm = word.search(letter.toLowerCase()) + 1;
-                obj = document.getElementById('word'+lettertm).innerHTML = letter.toUpperCase();
-                word = word.replace(letter,'0');
-                hits += 1;
-                search = word.match(letter.toLowerCase());
-                right = true;
-            }
-            document.getElementById('chosenLetters').innerHTML += `<span>${letter.toUpperCase()}  </span>`;
-            if (!right) {
-                mistakes += 1;
-                if (mistakes < 7) {
 
-                }else{
-                    playing = false;document.getElementById('msg').innerHTML = 'Infelizmente você perdeu ):! A palavra era : '+word1.toUpperCase()+'.';
-                    document.getElementById('msg').style.background = 'rgba(255,0,0,0.5)';
-                }
-            }
-            if (hits == wordWidth) {
-                playing = false;
-                document.getElementById('msg').innerHTML = 'Parabéns , você ganhou  A palavra era : '+word1.toUpperCase()+'.';
-                document.getElementById('msg').style.background = 'rgba(0,255,0,0.5)';
-            }
-            if (!playing){
-                document.getElementById('msg').style.display = 'block';
-                document.getElementById('chosenLetters').style.display = 'none';
-                document.getElementById('playeractions').style.display = 'none';
-            }
-        }
-    }
-}
+};
 
 function startGame() {
-    document.getElementById('msg').style.display = 'none';
-    document.getElementById('chosenLetters').style.display = 'block';
-    document.getElementById('playeractions').style.display = 'block';
+    playing = true ;
     document.getElementById('game').style.display = 'block';
-    document.getElementById('start').innerHTML = 'recomeçar jogo'
-    playing = true;
     play = document.getElementById('letter');
-    play.value='';
-    play.focus();
-    mistakes = 0;
-    hits = 0;
-    right = false;
-    document.getElementById('chosenLetters').innerHTML = 'Letras Digitadas : ';
-    usedClass = elements[position].class;
-    position = Math.round(Math.random()*(elements.length-1));
-    usedClass = elements[position].class
-    word = elements[position].element[Math.round(Math.random()*(elements[position].element.length-1))];
-    word1 = word;
-    wordWidth = word.length;
-    whatletters(wordWidth);
-    document.getElementById('step').innerHTML = usedClass;
-    document.getElementById('step').style.background = elements[position].color;
+    play.value = '' ;
+    play.focus() ;
+    hits = 0 ;
+    mistakes = 0 ;
+    hight = false ;
+    document.getElementById('chosenLetters').innerHTML = 'Letras digitadas : ' ;
+    position = Math.round(Math.random()*allClasses);
+    clas = elements[position].class ;
+    wordPosition = Math.round(Math.random()*(elements[position].element.length - 1)) ;
+    word = elements[position].element[wordPosition] ;
+    let step = document.getElementById('step') ;
+    step.innerHTML = clas ;
+    step.style.background = elements[position].color ;
+    wordLetters(word);
 };
 
 document.getElementById('start').addEventListener('click',startGame);
